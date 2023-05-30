03d3c640e8166125633101b74da7b15ddf9264cb
rafaelsantos95 <rafael-dev@hotmail.com>
2023-05-29 23:28:24 -0300

    removed Reservation type import from reservations/route

--- src/app/api/reservations/route.ts
+++ src/app/api/reservations/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from 'next/server'
 
 import prisma from '@/libs/prismadb'
 import getUser from '@/app/actions/getUser'
-import { Reservation } from '@prisma/client'
 
 export const POST = async (req: Request) => {
   const user = await getUser()
@@ -11,7 +10,7 @@ export const POST = async (req: Request) => {
     return NextResponse.error()
   }
 
-  const body: Reservation = await req.json()
+  const body = await req.json()
 
   const { listingId, startDate, endDate, totalPrice } = body
 
